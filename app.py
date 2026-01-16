230df727e2b84b06a7f533b90937374149803dfb
philmacedo <philipyborges@gmail.com>
2026-01-16 18:00:50 -0300

    Update: frontend

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="CS2 Hub", page_icon="🔫", layout="wide")
 st.markdown("""
 <style>
     /* Fundo geral ajustado */
-    .stApp { background-color: #de9b35; }
+    .stApp { background-color: #0e1012; }
     
     /* Cartões do Pódio */
     .podium-card {
